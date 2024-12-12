2c67cb397c18d9cc5679d9ba8e3e8e1172b7468b
naayifmuhammad <muhdnaayif@gmail.com>
2024-12-13 02:34:20 +0530

    Deployment build push

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000','https://real3dspacescan.com/'],
   credentials: true,
 }));
 
